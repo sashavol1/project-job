6207c37a7f77dbb73c461b487786e252810da0b1
Alexandr Volmanov <C:\Users\al\AppData\Roaming\The Bat!>
2020-04-10 22:57:34 +0300

    новая ветка

--- www/app/View/index/index.php
+++ www/app/View/index/index.php
@@ -63,30 +63,7 @@
             </ul>
         </div>
         <div class="col-sm-8">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>
+            // Тут будет список вакансий работы
         </div>
     </div>
 </div>
